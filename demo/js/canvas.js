7baaf08a41c9d233ce6286105cc23ec237f6527f
Elias Schütt <contact@elias-schuett.de>
2014-10-04 20:13:35 +0200

    Updated the canvas example

--- demo/js/canvas.js
+++ demo/js/canvas.js
@@ -1,45 +1,67 @@
 ;(function(window, undefined) {
 
-	var ctx = document.querySelector("canvas").getContext("2d");
+	var ctx = document.querySelector("canvas").getContext("2d"),
+	
+	    W = 320,
+	    H = 180,
 
-	var W = 320;
-	var H = 180;
+	    imageData = ctx.createImageData(W,H),
 
-	var imageData = ctx.createImageData(W,H),
+	    d = imageData.data,
+	    N = 300,
+	    a = 0,
 
-		d = imageData.data,
-	    N = 600,
-	    CX = -1.7451606879398844,
-	    CY = -9.383515877032168e-12,
-	    CZ = 2.9802322387695312e-8,
+	    CX = -1.77810334274064037110522326038852639499207961414628307584575173232969154440,
+	    CY = 0.00767394242121339392672671947893471774958985018535019684946671264012302378,
+	    CZ = 8,
 
-	    r,i,t,n,_x,_y;
+	    interval;
 
-	for(var y = 0; y < H; y++) {
+	function render() {
 
-		_y = (y/H-0.5)*CZ*H/W+CY;
+		var r,i,t,n,_x,_y,__x,__y;
 
-		for(var x = 0; x < W; x++) {
+		a += 0.01;
+		CZ *= 0.9;
 
-			_x = (x/W-0.5)*CZ+CX;
+		for(var y = 0; y < H; y++) {
 
-			r=i=t=0;n=N;
+			_y = (y/H-0.5)*CZ*H/W+CY;
+			__y = _y;
 
-			while(r*r+i*i<128&&n--) {
-				r = r*r-i*i+_x;
-				i = 2*t*i+_y;t=r;
+			for(var x = 0; x < W; x++) {
+
+				_x = (x/W-0.5)*CZ+CX;
+				__x = _x;
+
+				_x = CX + (__x-CX)*Math.cos(a) - (__y-CY)*Math.sin(a);
+				_y = CY + (__x-CX)*Math.sin(a) + (__y-CY)*Math.cos(a);
+
+				r=i=t=0;n=N;
+
+				while(r*r+i*i<128&&n--) {
+					r = r*r-i*i+_x;
+					i = 2*t*i+_y;t=r;
+				}
+				
+				n=n>0?(N-n-Math.log(Math.log(r*r+i*i)/Math.log(2))/Math.log(2))*0.1:0;
+				i = (y*W+x)*4;
+
+				d[i++] = n&&Math.sin(n+120)*255;
+				d[i++] = n&&Math.sin(n+240)*255;
+				d[i++] = n&&Math.sin(n+360)*255;
+				d[i++] = 255;
 			}
-			
-			n=n>0?(N-n-Math.log(Math.log(r*r+i*i)/Math.log(2))/Math.log(2))*0.05:0;
-			i = (y*W+x)*4;
-
-			d[i++] = n&&Math.sin(n+120)*255;
-			d[i++] = n&&Math.sin(n+240)*255;
-			d[i++] = n&&Math.sin(n+360)*255;
-			d[i++] = 255;
 		}
+
+		ctx.putImageData(imageData,0,0);
+
+		if (CZ <= 1e-10) { CZ = 4; }
 	}
 
-	ctx.putImageData(imageData,0,0);
+	ctx.canvas.onmouseover = function() { interval = window.setInterval(render, 1000 / 60); };
+	ctx.canvas.onmouseout  = function() { window.clearInterval(interval); };
+
+	render();
 
 }(window));
